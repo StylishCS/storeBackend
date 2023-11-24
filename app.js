202a17086a2f2d3f8e37224caf95a0104b531df5
StylishCS <joeshirf@gmail.com>
2023-11-24 08:03:22 +0200

    cors

--- app.js
+++ app.js
@@ -25,16 +25,20 @@ mongoose
 var app = express();
 app.use(bodyParser.json({ limit: "2000kb" }));
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-app.use(expressCookie({
-  sameSite: true,
-}));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 
 // view engine setup
